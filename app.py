6f992347ee1e4ec48e5e14e597bf2580f7d9b7a9
ytgenchev24-blip <ytgenchev24@gmail.com>
2026-01-22 10:37:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("Log in system")
 name = st.text_input("Въведи име")
 if st.button("Провери"):
-if name.strip() ==" ":
+if name.strip() == " ":
 st.warning ("Моля въведи текст")
 elif not name is alpha():
 st.warning("------")
